2f5348103d3707590f8f86e43a92b1507987084b
student <sonamt11@nycstudents.net>
2023-03-27 12:28:39 -0400

    commit

---
+++ src/CelebrityRunner.java
@@ -0,0 +1,16 @@
+/**
+ * Starts the Celebrity Game application
+ * @author cody.henrichsen
+ *
+ */
+public class CelebrityRunner {
+	/**
+	 * The entry point of the Java application.
+	 * @param args Unused parameter as it is a GUI based program.
+	 */
+	public static void main(String [] args) {
+		Celebrity cel = new Celebrity("Big Bird", "From Sesame Street, covered in yellow feathers");
+		System.out.println(cel); // test toString
+
+	}
+}
